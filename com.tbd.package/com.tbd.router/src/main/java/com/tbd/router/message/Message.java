c407037b0c18e543c386f0664d2f09efcde4534b
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2011-08-08 23:39:59 -0700

    First commit

---
+++ com.tbd.package/com.tbd.router/src/main/java/com/tbd/router/message/Message.java
@@ -0,0 +1,18 @@
+package com.tbd.router.message;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: abhinavtrivedi
+ * Date: 8/7/11
+ * Time: 11:49 PM
+ */
+
+/*
+* Message is the common contract between the acceptor and connector
+*
+ */
+public interface Message {
+
+
+
+}
